703e9290bd44e7378521d911aa355864ef198ccb
Reinielle Matt <erecereinielle722@gmail.com>
2019-05-25 14:24:27 +0800

    Views and ViewModels Created
    The main views that are needed and there corresponding view models have been created. There layouts and important elements are added as well.

--- TournamentTrackerUI/ViewModels/ShellViewModel.cs
+++ TournamentTrackerUI/ViewModels/ShellViewModel.cs
@@ -4,5 +4,9 @@ namespace TournamentTrackerUI.ViewModels
 {
     public class ShellViewModel : Conductor<object>
     {
+        public ShellViewModel(TournamentViewModel viewModel)
+        {
+            ActivateItem(viewModel);
+        }
     }
 }
---
+++ TournamentTrackerUI/ViewModels/TournamentViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Caliburn.Micro;
+
+namespace TournamentTrackerUI.ViewModels
+{
+    public class TournamentViewModel : Screen
+    {
+    }
+}
